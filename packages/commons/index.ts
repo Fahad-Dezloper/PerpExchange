1584c2042f0d1727d7b087d6d9e466127e780566
Fahad <fahad.khan2216@gmail.com>
2026-06-13 23:08:40 +0530

    feat: Init few endpoints initialized engine and types

---
+++ packages/commons/index.ts
@@ -0,0 +1,26 @@
+export type ToEngine =
+  | {
+      messageType: "onramp";
+      userId: string;
+      amount: number;
+    }
+  | {
+      messageType: "create_order";
+      price: string;
+      qty: string;
+      side: "short" | "long";
+      marketId: string;
+      type: "limit" | "market";
+      userId: string;
+      equity: string;
+      orderId: string;
+    }
+  | {
+      messageType: "cancel_order";
+      orderId: string;
+      userId: string;
+    }
+  | {
+      messageType: "create_market";
+      marketId: string;
+    };
