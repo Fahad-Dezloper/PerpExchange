87925aa4e070c91699a6786fe74c2f40ca1bf17d
Fahad <fahad.khan2216@gmail.com>
2026-06-14 13:47:43 +0530

    Feat: funding rate primary calculation function

--- apps/engine/index.ts
+++ apps/engine/index.ts
@@ -35,8 +35,23 @@ interface Orderbook {
 
 const orderbooks: Orderbook[] = [];
 const balances: Map<string, { available: string; locked: string }> = new Map();
+const positions: Map<
+  string,
+  Map<
+    string,
+    {
+      side: "long" | "short";
+      averagePrice: number;
+      qty: string;
+      liquidationPrice: string;
+      stopLoss: string;
+      takeProfit: string;
+      equity: string;
+    }
+  >
+> = new Map();
 
-function matching() {
+async function matching() {
   while (1) {
     const response = await client.xReadGroup(
       "engine",
@@ -59,7 +74,7 @@ function matching() {
 
     const message: {
       loopBackId: string;
-    } & ToEngine = response[0].messages[0].message;
+    } & ToEngine = response[0]!.messages[0].message;
 
     if (message.messageType == "create_market") {
       orderbooks.push({
@@ -75,21 +90,72 @@ function matching() {
     }
 
     if (message.messageType === "onramp") {
-      balances[message.userId].available += message.amount;
+      balances.get(message.userId)!.available += message.amount;
       await publisher.xAdd("to-backend", "*", {
         loopBackId: message.loopBackId,
       });
     }
 
     if (message.messageType === "create_order") {
+      // create a new order, match if possible
     }
 
     if (message.messageType === "cancel_order") {
+      //  cancel an open order
+    }
+
+    if (message.messageType === "get_depth") {
+      //  get the depth for a market
+    }
+
+    if (message.messageType === "spot_price_update") {
+      // do liquidation checks, stop loss and take profit
+    }
+
+    if (message.messageType === "get_funding_rate") {
+      // get the funding rate based on the diff b/w mark price and last traded price
     }
   }
 }
 
-function liquidationChecks() {}
+function fundingRateDespersal() {
+  // check the mark price, calculate how far it is from the last traded price
+  // longs pay shortts or vice versa
+  // liquidation price changes for all positions
+  positions.forEach((userPositions, userId) => {
+    userPositions.forEach((position, marketId) => {
+      const orderbook = orderbooks.get(marketId);
+      if (!orderbook) return;
+
+      const inflationRate =
+        (Number(orderbook.lastTradedPrice) - Number(orderbook.markPrice)) /
+        Number(orderbook.markPrice);
+
+      if (position.side === "long") {
+        const notionalValue =
+          Number(position.qty) * Number(orderbook.lastTradedPrice);
+        position.equity = (
+          Number(position.equity) -
+          notionalValue * inflationRate
+        ).toString();
+        // recalculate liquidation price
+      } else {
+        const notionalValue =
+          Number(position.qty) * Number(orderbook.lastTradedPrice);
+        position.equity += (
+          Number(position.equity) +
+          notionalValue * inflationRate
+        ).toString();
+        // recalculate liquidation price
+      }
+    });
+  });
+}
 
 matching();
-liquidationChecks();
+setInterval(
+  () => {
+    fundingRateDespersal();
+  },
+  8 * 60 * 60 * 1000,
+);
